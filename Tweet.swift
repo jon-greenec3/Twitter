d13b373b295d6f16906cedf3f08422eae84be09c
jon-greenec3 <jon.greenec3@gmail.com>
2016-03-02 23:09:03 -0600

    Update Tweet.swift

--- Tweet.swift
+++ Tweet.swift
@@ -1,48 +1,83 @@
 //
 //  Tweet.swift
-//  TwitterDemo
+//  Twitter2
 //
-//  Created by Windell Greene on 2/20/16.
-//  Copyright © 2016 codepath. All rights reserved.
+//  Created by Jacob Hughes on 2/21/16.
+//  Copyright © 2016 Jacob Hughes. All rights reserved.
 //
 
 import UIKit
 
 class Tweet: NSObject {
-    
     var text: NSString?
-    var timestamp: NSDate?
+    var timeStamp: NSDate?
     var retweetCount: Int = 0
     var favoritesCount: Int = 0
+    var bannerPic: NSURL?
+    
+    var userName: String?
+    var userHandle: String?
+    var imageProfileURL: NSURL?
+    
+    var favorited: Int = 0
+    var retweeted: Int = 0
+    
+    var user: User
+    
     
     init(dictionary: NSDictionary) {
+        
+        
+        self.user = User(dictionary: dictionary["user"] as! NSDictionary)
         text = dictionary["text"] as? String
-        
         retweetCount = (dictionary["retweet_count"] as? Int) ?? 0
-        favoritesCount = (dictionary["favourites_count"] as? Int) ?? 0
+        favoritesCount = (dictionary["favorite_count"] as? Int) ?? 0
+        print(dictionary)
         
-        let timestampString = dictionary["created_at"] as? String
+        let timesStampString = dictionary["created_at"] as? String
         
-        if let timestampString = timestampString {
+        if let timesStampString = timesStampString{
             let formatter = NSDateFormatter()
             formatter.dateFormat = "EEE MMM d HH:mm:ss Z y"
-            timestamp = formatter.dateFromString(timestampString)
+            
+            //self.user = dictionary["user"]
+            
+            timeStamp = formatter.dateFromString(timesStampString)
         }
+        
+        let user = dictionary["user"]
+        
+        
+        
+        //user name labels
+        userName = user!["name"] as? String
+        userHandle = user!["screen_name"] as? String
+        
+        //user picture
+        imageProfileURL = NSURL(string: (user!["profile_image_url"] as! String))
+        //bannerPic = NSURL(string: (user!["profile_banner_url"] as! String))
+        
+        
+        //user actions
+        favorited = dictionary["favorited"] as! Int
+        retweeted = dictionary["retweeted"] as! Int
+        
+        //tweetNumber = user!["count"]
+        
+        print(favorited)
+        print(retweeted)
+        
+        
         
     }
     
-    class func tweetsWithArray(dictionaries: [NSDictionary]) -> [Tweet] {
+    class func TweetsWithArray(dictionaries: [NSDictionary]) -> [Tweet]{
         var tweets = [Tweet]()
         
-        for dictionary in dictionaries {
-            let tweet = Tweet(dictionary: dictionary)
-            
+        for dictionary in dictionaries{
+            let tweet  = Tweet(dictionary: dictionary)
             tweets.append(tweet)
         }
-        
         return tweets
-    
-    
     }
-
 }
